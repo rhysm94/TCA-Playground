6aca2d1bf7d0031297b780d36611ea01d126e781
Rhys Morgan <rhysm@me.com>
2021-02-02 21:59:58 +0000

    Initial Commit

---
+++ Shared/TCA_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PlaygroundApp.swift
+//  Shared
+//
+//  Created by Rhys Morgan on 02/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
